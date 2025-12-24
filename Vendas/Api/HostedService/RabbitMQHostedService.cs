8fcc6f74f7f1afbd183fb98198b3d35547618208
Poopstoop1 <daniel.026@hotmail.com>
2025-12-23 22:46:39 -0300

    Refactor(Vendas): Refatorando RabbitMQClient, Adicionando IAsyncDisposable atualizando dependencia e HostedService.

--- Vendas/Api/HostedService/RabbitMQHostedService.cs
+++ Vendas/Api/HostedService/RabbitMQHostedService.cs
@@ -1,21 +1,29 @@
-﻿
-using Application.Interfaces;
-
+﻿using Application.Interfaces;
+using Infrastructure.MessageBus;
 
 namespace Vendas.HostedService
 {
-    public class RabbitmqHostedService(IEnumerable<IConsumer> consumers) : BackgroundService
+    public class RabbitmqHostedService(RabbitMQClient bus, IEnumerable<IConsumer> consumers) : BackgroundService
     {
+        private readonly RabbitMQClient _bus = bus;
         private readonly IEnumerable<IConsumer> _consumers = consumers;
-
-        
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await _bus.StartAsync(stoppingToken);
+
             var tasks = _consumers
                 .Select(c => c.StartAsync(stoppingToken));
 
             await Task.WhenAll(tasks);
+
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _bus.DisposeAsync();
+            await base.StopAsync(cancellationToken);
         }
     }
 }
